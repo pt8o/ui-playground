f7282ae11cd6212e81145205833e29cd8da2133b
superko <lucas@superko.org>
2018-04-25 18:34:35 -0400

    general cleanup

--- src/client/components/index.jsx
+++ src/client/components/index.jsx
@@ -13,27 +13,45 @@ const options = {
         'mockProps': ['contact={contactObject} OR username="username"'],
         'bools': ['clickable', 'tooltip']
     },
+
     'Button': {
         'textProps': ['label', 'icon', 'customIcon', 'theme', 'tooltip', 'tooltipPosition', 'tooltipSize'],
         'bools': ['disabled', 'selected', 'active']
     },
+
     'Checkbox': {
         'textProps': ['label'],
         'bools': ['disabled']
     },
+
     'Chip': {
         'textProps': [],
         'bools': ['deletable'],
         'childContent': 'Child content'
     },
+
     'CustomIcon': {'textProps': [], 'bools': []},
+
     'Dialog': {
         'textProps': ['theme', 'title'],
-        'mockProps': ['active={boolean}', 'onCancel={function}', 'actions={buttonsGoHere}'],
+        'mockProps': [
+            'active={boolean}',
+            'onCancel={function}',
+            'actions={[{ label: string, onClick: function }, ...]}'
+        ],
         'bools': ['active', 'noAnimation'],
         'childContent': 'Any HTML content can go in here'
     },
-    'Dropdown': {'textProps': [], 'bools': []},
+
+    'Dropdown': {
+        'textProps': [],
+        'mockProps': [
+            'value="string"',
+            'options={[{ value: string, label: string }, ...]}'
+        ],
+        'bools': []
+    },
+
     'Input': {'textProps': [], 'bools': []},
     'List': {'textProps': [], 'bools': []},
     'MaterialIcon': {'textProps': [], 'bools': []},
