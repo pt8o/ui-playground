57851ee382130f5074b29727f5d9c6b4f2a72d56
superko <lucas@superko.org>
2018-04-25 16:54:53 -0400

    WIP various components

--- src/client/components/ComponentPlayground.jsx
+++ src/client/components/ComponentPlayground.jsx
@@ -33,6 +33,8 @@ const contact = {
 
 @observer
 class ComponentPlayground extends React.Component {
+    @observable dropdownValue = '';
+
     constructor() {
         super();
         this.propertyList = observable.map();
@@ -43,7 +45,7 @@ class ComponentPlayground extends React.Component {
         });
     }
 
-    @computed get properties() {
+    get properties() {
         const obj = {};
         const selected = this.props.options[this.props.selected];
 
@@ -54,7 +56,7 @@ class ComponentPlayground extends React.Component {
         return obj;
     }
 
-    @computed get codeBlock() {
+    get codeBlock() {
         const block = [];
         const selected = this.props.options[this.props.selected];
 
@@ -100,6 +102,39 @@ class ComponentPlayground extends React.Component {
                         {this.props.selected === 'Avatar' && <Avatar contact={contact} {...this.properties} /> }
                         {this.props.selected === 'Button' && <Button {...this.properties} />}
                         {this.props.selected === 'Checkbox' && <Checkbox {...this.properties} />}
+                        {this.props.selected === 'Chip' && <Chip {...this.properties} />}
+                        {this.props.selected === 'CustomIcon' && <CustomIcon {...this.properties} />}
+                        {this.props.selected === 'Dialog' && <Dialog {...this.properties} />}
+                        {this.props.selected === 'Dropdown' &&
+                            <Dropdown
+                                onChange={(value) => this.dropdownValue = value}
+                                value={this.dropdownValue || 'item1'}
+                                options={[
+                                    { value: 'item1', label: 'First item' },
+                                    { value: 'item2', label: 'Second item' },
+                                    { value: 'item3', label: 'Third item' },
+                                    { value: 'item4', label: 'Fourth item' }
+                                ]}
+                                {...this.properties}
+                            />
+                        }
+                        {this.props.selected === 'Input' && <Input {...this.properties} />}
+                        {this.props.selected === 'List' &&
+                            <List {...this.properties}>
+                                <ListHeading>List Heading</ListHeading>
+                                <ListItem>Item 1</ListItem>
+                                <ListItem>Item 2</ListItem>
+                                <ListItem>Item 3</ListItem>
+                                <ListHeading>List Heading</ListHeading>
+                                <ListItem>Item 4</ListItem>
+                                <ListItem>Item 5</ListItem>
+                            </List>
+                        }
+                        {this.props.selected === 'MaterialIcon' && <MaterialIcon {...this.properties} />}
+                        {this.props.selected === 'Menu' && <Menu {...this.properties} />}
+                        {this.props.selected === 'ProgressBar' && <ProgressBar {...this.properties} />}
+                        {this.props.selected === 'RadioButtons' && <RadioButtons {...this.properties} />}
+                        {this.props.selected === 'Switch' && <Switch {...this.properties} />}
                     </div>
                 </div>
             </div>
