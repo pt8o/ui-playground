e4e7c592496ddbaf3f91fc9802cbda3c7cdff498
superko <lucas@superko.org>
2018-04-26 18:52:17 -0400

    components.js cleanup

--- src/client/components/data/components.js
+++ src/client/components/data/components.js
@@ -1,96 +1,156 @@
 const components = {
-    'Avatar': {
-        'textProps': ['size'],
-        'mockProps': ['contact={contactObject} OR username="username"'],
-        'bools': ['clickable', 'tooltip']
+    Avatar: {
+        textProps: ['size'],
+        mockProps: ['contact={contactObject} OR username="username"'],
+        bools: ['clickable', 'tooltip'],
+
+        instructions: [
+            'Test'
+        ]
     },
 
-    'Button': {
-        'textProps': ['label', 'icon', 'customIcon', 'theme', 'tooltip', 'tooltipPosition', 'tooltipSize'],
-        'bools': ['disabled', 'selected', 'active']
+    Button: {
+        textProps: ['label', 'icon', 'customIcon', 'theme', 'tooltip', 'tooltipPosition', 'tooltipSize'],
+        bools: ['disabled', 'selected', 'active'],
+
+        instructions: [
+            ''
+        ]
     },
 
-    'Checkbox': {
-        'textProps': ['label'],
-        'bools': ['disabled']
+    Checkbox: {
+        textProps: ['label'],
+        bools: ['disabled'],
+
+        instructions: [
+            ''
+        ]
     },
 
-    'Chip': {
-        'textProps': [],
-        'bools': ['deletable'],
-        'childContent': 'Child content'
+    Chip: {
+        textProps: [],
+        bools: ['deletable'],
+        'childContent': 'Child content',
+
+        instructions: [
+            ''
+        ]
     },
 
-    'CustomIcon': {
-        'textProps': ['icon', 'size'],
-        'mockProps': ['hover={boolean // see instructions}'],
-        'bools': ['selected', 'active']
+    CustomIcon: {
+        textProps: ['icon', 'size'],
+        mockProps: ['hover={boolean // see instructions}'],
+        bools: ['selected', 'active'],
+
+        instructions: [
+            ''
+        ]
     },
 
-    'Dialog': {
-        'textProps': ['theme', 'title'],
-        'mockProps': [
+    Dialog: {
+        textProps: ['theme', 'title'],
+        mockProps: [
             'active={boolean}',
             'onCancel={function}',
             'actions={[{ label: string, onClick: function }, ...]}'
         ],
-        'bools': ['noAnimation'],
-        'childContent': 'Any HTML content can go in here'
+        bools: ['noAnimation'],
+        'childContent': 'Any HTML content can go in here',
+
+        instructions: [
+            ''
+        ]
     },
 
-    'Dropdown': {
-        'textProps': ['label'],
-        'mockProps': [
+    Dropdown: {
+        textProps: ['label'],
+        mockProps: [
             'value="string"',
             'options={[{ value: string, label: string }, ...]}',
             'onChange={function}'
         ],
-        'bools': []
+        bools: [],
+
+        instructions: [
+            ''
+        ]
     },
 
-    'Input': {
-        'textProps': ['type', 'placeholder', 'label', 'hint', 'error', 'maxLength'],
-        'mockProps': ['onBlur={function}', 'onFocus={function}', 'onChange={function}', 'onKey[Press/Up/Down]={function}'],
-        'bools': ['autofocus', 'disabled', 'readOnly', 'multiline']
+    Input: {
+        textProps: ['type', 'placeholder', 'label', 'hint', 'error', 'maxLength'],
+        mockProps: ['onBlur={function}', 'onFocus={function}', 'onChange={function}', 'onKey[Press/Up/Down]={function}'],
+        bools: ['autofocus', 'disabled', 'readOnly', 'multiline'],
+
+        instructions: [
+            ''
+        ]
     },
 
-    'ListItem': {
-        'textProps': ['caption', 'legend', 'leftContent', 'leftIcon', 'rightContent', 'rightIcon'],
-        'mockProps': ['onClick="{function}"'],
-        'bools': ['disabled']
+    ListItem: {
+        textProps: ['caption', 'legend', 'leftContent', 'leftIcon', 'rightContent', 'rightIcon'],
+        mockProps: ['onClick="{function}"'],
+        bools: ['disabled'],
+
+        instructions: [
+            ''
+        ]
     },
 
-    'MaterialIcon': {
-        'textProps': ['icon', 'tooltip', 'tooltipPosition'],
-        'bools': ['active']
+    MaterialIcon: {
+        textProps: ['icon', 'tooltip', 'tooltipPosition'],
+        bools: ['active'],
+
+        instructions: [
+            ''
+        ]
     },
 
-    'Menu': {
-        'textProps': ['position', 'icon', 'customIcon', 'customButton', 'tooltip', 'tooltipPosition'],
-        'bools': ['disabled']
+    Menu: {
+        textProps: ['position', 'icon', 'customIcon', 'customButton', 'tooltip', 'tooltipPosition'],
+        bools: ['disabled'],
+
+        instructions: [
+            ''
+        ]
     },
 
-    'MenuItem': {
-        'textProps': ['caption', 'icon', 'customIcon'],
-        'mockProps': ['value="string"'],
-        'bools': []
+    MenuItem: {
+        textProps: ['caption', 'icon', 'customIcon'],
+        mockProps: ['value="string"'],
+        bools: [],
+
+        instructions: [
+            ''
+        ]
     },
 
-    'ProgressBar': {
-        'textProps': ['mode', 'type', 'theme', 'value', 'max'],
-        'bools': []
+    ProgressBar: {
+        textProps: ['mode', 'type', 'theme', 'value', 'max'],
+        bools: [],
+
+        instructions: [
+            ''
+        ]
     },
 
-    'RadioButtons': {
-        'textProps': [],
-        'mockProps': ['value="string"', 'options=[{ value: string, label: string } ...]', 'onChange="function"'],
-        'bools': []
+    RadioButtons: {
+        textProps: [],
+        mockProps: ['value="string"', 'options=[{ value: string, label: string } ...]', 'onChange="function"'],
+        bools: [],
+
+        instructions: [
+            ''
+        ]
     },
 
-    'Switch': {
-        'textProps': ['label'],
-        'mockProps': ['checked={bool}', 'onChange={function}'],
-        'bools': []
+    Switch: {
+        textProps: ['label'],
+        mockProps: ['checked={bool}', 'onChange={function}'],
+        bools: [],
+
+        instructions: [
+            ''
+        ]
     }
 };
 
